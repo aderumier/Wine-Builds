3ba428563e68873d6f3343beaeba8df92ba5d0c9
Alexandre Derumier <aderumier@gmail.com>
2026-02-26 15:02:40 +0100

    Pin vanilla and staging to Wine 10.2 & keep tkg 10.20

--- build_wine.sh
+++ build_wine.sh
@@ -208,7 +208,7 @@ if [ -n "${CUSTOM_SRC_PATH}" ]; then
 	BUILD_NAME="${WINE_VERSION}"-custom
 elif [ "$WINE_BRANCH" = "staging-tkg" ] || [ "$WINE_BRANCH" = "staging-tkg-fsync" ]; then
 	if [ "$WINE_BRANCH" = "staging-tkg" ]; then
-		git clone https://github.com/Kron4ek/wine-tkg wine -b 9.17
+		git clone https://github.com/Kron4ek/wine-tkg wine -b 10.20
 	else
 		git clone https://github.com/Kron4ek/wine-tkg wine -b fsync
 	fi
